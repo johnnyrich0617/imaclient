29db79880a7fad90f860fd2c6652f4f2467c9d5d
jrichardson0617 <john.richardson.0617@gmail.com>
2021-06-15 16:04:00 -0400

    Alerts Service

--- src/app/services/alert.service.ts
+++ src/app/services/alert.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
-import {Observable} from 'rxjs';
-import {Alert} from '../models/alert.model';
+import {Observable, Subject} from 'rxjs';
+import { Alert, AlertType } from '../models/alert.model';
 import {filter} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AlertService {
-  private defaultId: any;
-  private subject: any;
+  private defaultId = 'default-alert';
+  private subject = new Subject<Alert>();
 
   constructor() { }
 
@@ -17,7 +17,31 @@ export class AlertService {
     return this.subject.asObservable().pipe(filter(x => x && x.id === id));
   }
 
-  clear(id: string): void {
-
+  // convenience methods
+  success(message: string, options?: any): void {
+    this.alert(new Alert({ ...options, type: AlertType.Success, message }));
   }
+
+  error(message: string, options?: any): void {
+    this.alert(new Alert({ ...options, type: AlertType.Error, message }));
+  }
+
+  info(message: string, options?: any): void {
+    this.alert(new Alert({ ...options, type: AlertType.Info, message }));
+  }
+
+  warn(message: string, options?: any): void {
+    this.alert(new Alert({ ...options, type: AlertType.Warning, message }));
+  }
+  // main alert method
+  alert(alert: Alert): void {
+    alert.id = alert.id || this.defaultId;
+    this.subject.next(alert);
+  }
+
+  // clear alerts
+  clear(id = this.defaultId): void {
+    this.subject.next(new Alert({ id }));
+  }
+
 }
